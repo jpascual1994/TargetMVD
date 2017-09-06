aad03f3527824b02f99db33df8add3598e4633cd
Juan Pascual <jp.pascual@rootstrap.com>
2017-09-06 10:56:04 -0300

    Fix feature sign in

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -7,45 +7,61 @@ RSpec.feature 'Sign In', type: :feature, js: true do
     visit root_path
   end
 
-  context 'with valid params' do
-    scenario 'Sucessful sign in' do
-      user.confirm
-      page.fill_in 'user_email', with: user.email
-      page.fill_in 'user_password', with: '123456'
-      click_button  "Sign in"
-      expect(page).to have_current_path(homepage_users_path)
-      expect(page).to have_content( user.name )
+  context 'With valid params' do
+    context 'Sucessful sing in' do
+      background do
+        user.confirm
+        fill_form_and_click user.email, '123456'
+      end
+
+      scenario 'Redirect to homepage' do
+        expect(page).to have_current_path(homepage_users_path)
+      end
+
+      scenario 'Homepage show user name' do
+        expect(page).to have_content(user.name)
+      end
     end
 
-    scenario 'Unconfirmed user' do
-      page.fill_in 'user_email', with: user.email
-      page.fill_in 'user_password', with: '123456'
-      click_button  "Sign in"
-      expect(page).to have_current_path(new_user_session_path)
-      expect(page).to have_content( 'You have to confirm your email address before continuing.' )
+    context 'Unconfirmed user' do
+      background do
+        fill_form_and_click user.email, '123456'
+      end
+
+      scenario 'Show unconfirmed message' do
+        expect(page).to have_content('You have to confirm your email address before continuing.')
+      end
+
+      scenario 'Unconfirmed user redirect to root' do
+        expect(page).to have_current_path(root_path)
+      end
     end
   end
 
-  context 'with invalid params' do
+  context 'With invalid params' do
     before(:each) do
       user.confirm
     end
 
-    after(:each) do
-      expect(page).to have_current_path(new_user_session_path)
-      expect(page).to have_content( 'Invalid Email or password' )
-    end
-
     scenario 'Invalid Password' do
-      page.fill_in 'user_email', with: user.email
-      page.fill_in 'user_password', with: '654321'
-      click_button  "Sign in"
+      fill_form_and_click user.email, '65421'
+      expect(page).to have_content('Invalid Email or password')
     end
 
     scenario 'Invalid Email' do
-      page.fill_in 'user_email', with: ''
-      page.fill_in 'user_password', with: '123456'
-      click_button  "Sign in"
+      fill_form_and_click '', '123456'
+      expect(page).to have_content('You need to sign in or sign up before continuing')
     end
+
+    scenario 'Redirect to root' do
+      fill_form_and_click user.email, '65421'
+      expect(page).to have_current_path(root_path)
+    end
+  end
+
+  def fill_form_and_click(email, password)
+    page.fill_in 'user_email', with: email
+    page.fill_in 'user_password', with: password
+    click_button  'Sign in'
   end
 end
